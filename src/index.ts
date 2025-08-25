3e25cbc9516a1a2fc789fce43be06db5b7e6ad2d
Naman Mittal <namanmittal501@gmail.com>
2025-08-25 16:29:35 +0530

    Add TypeScript definitions for various packages and set up a basic Express server
    - Added @types/mime package with version 1.3.5 for TypeScript definitions.
    - Added @types/qs package with version 6.14.0 for TypeScript definitions.
    - Added @types/range-parser package with version 1.2.7 for TypeScript definitions.
    - Added @types/send package with version 0.17.5 for TypeScript definitions.
    - Added @types/serve-static package with version 1.15.8 for TypeScript definitions.
    - Updated package-lock.json to include new type definitions and their dependencies.
    - Created a basic Express server in src/index.ts that listens on port 8080.
    - Updated tsconfig.json to enable esModuleInterop for better module compatibility.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("NAMAN");
+// CREATING A SERVER
+// STEPS
+// 1. Import the express
+
+import { log } from "console";
+import express,{Express} from "express";
+
+// 2. Initialize the express
+
+    const app:Express = express()
+
+// 3. Define a port
+
+const port = 8080
+
+// 4.  Listen on a port
+
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`);  
+})
