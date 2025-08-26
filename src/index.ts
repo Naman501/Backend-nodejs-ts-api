7c4e0822b2e98823aab859a2ecff404902f7e72c
Naman Mittal <namanmittal501@gmail.com>
2025-08-26 16:45:05 +0530

    Add @types/cors package and integrate CORS middleware in the Express server

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 import dotenv from "dotenv";
 import {config} from "dotenv";
-// CREATING A SERVER
-// STEPS
-// 1. Import the express
+
 
 import { log } from "console";
 import express,{Express,Request,Response} from "express";
-
-// 2. Initialize the express
-
-    const app:Express = express()
+import cors from "cors"
 
 
-    config();
-// 3. Define a port
+
+const app:Express = express()
+
+config();
 
 const port = process.env.PORT || 8080
+
+
+// Middleware
+app.use(cors({
+    origin:process.env.HOST_URL || "*" 
+}))
 
 
 //BASIC ENDPOINT
@@ -29,7 +32,6 @@ app.get("/",(req:Request,res:Response)=>{
     })
 })
 
-// 4.  Listen on a port
 
 app.listen(port,()=>{
     console.log(`Server is running on ${port}`);  
